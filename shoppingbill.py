8302a2b19c5e037b67a79d470718d41c0eb0c2a7
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-18 22:18:50 +0530

    Add files via upload

---
+++ shoppingbill.py
@@ -0,0 +1,9 @@
+notebook = 3
+notebook_price = 45
+pen = 2
+pen_price = 20
+total_bill_amount = (notebook * notebook_price) + (pen * pen_price)
+print(total_bill_amount)
+amount_given = 500
+remaining_balance = (amount_given - total_bill_amount)
+print(remaining_balance)
