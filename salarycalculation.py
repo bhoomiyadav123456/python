15427a4d6741adcedf7a480059d9cb166eb26ea6
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-18 22:59:29 +0530

    Add files via upload

--- salarycalculation.py
+++ salarycalculation.py
@@ -1,1 +1,8 @@
-
+basic_salary = 25000
+hra = (20 * basic_salary) /100
+bonus = 5000
+total_salary = basic_salary + hra + bonus
+print("Basic Salary: - salarycalculation.py:5",basic_salary)
+print("HRA - salarycalculation.py:6",hra)
+print("Bonus: - salarycalculation.py:7",bonus)
+print("Total Salary: - salarycalculation.py:8",total_salary)
