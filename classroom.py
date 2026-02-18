2dca8b3c2a27935d687972fe6d36652eda5b0e66
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-18 23:59:22 +0530

    Add files via upload

---
+++ classroom.py
@@ -0,0 +1,15 @@
+# Total students
+total_students = 125
+
+# Students per class
+students_per_class = 30
+
+# Calculate full classes
+full_classes = total_students // students_per_class
+
+# Calculate remaining students
+remaining_students = total_students % students_per_class
+
+# Display result
+print("Full classes formed: - classroom.py:14", full_classes)
+print("Students remaining without class: - classroom.py:15", remaining_students)
