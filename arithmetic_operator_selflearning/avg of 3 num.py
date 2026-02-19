6bc45ae424c9dfc26eda4b51cdf137cd5a91c3e1
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:21:32 +0530

    Add files via upload

---
+++ arithmetic_operator_selflearning/avg of 3 num.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+average = (a + b + c) / 3
+print("Average is: - avg of 3 num.py:6", average)
