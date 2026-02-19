36ee054a41630d92c7167f74e7876dd274a38d24
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:43:30 +0530

    Add files via upload

---
+++ selection_statement_selflearning/check +,- or 0.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number - check +,- or 0.py:4")
+elif num < 0:
+    print("Negative number - check +,- or 0.py:6")
+else:
+    print("Zero - check +,- or 0.py:8")
