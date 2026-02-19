88118566d868c677b4fa1a618efcd810bf68f343
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:29:52 +0530

    Add files via upload

---
+++ arithmetic_operator_selflearning/swap two number.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping: - swap two number.py:4")
+print("a = - swap two number.py:5", a)
+print("b = - swap two number.py:6", b)
+
+# Swapping without third variable
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping: - swap two number.py:13")
+print("a = - swap two number.py:14", a)
+print("b = - swap two number.py:15", b)
