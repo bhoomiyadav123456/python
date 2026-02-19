8d2b38b854233e2871d4a1107c6319a273f5ae12
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:53:40 +0530

    Add files via upload

---
+++ selection_statement_selflearning/divisible by 5 and11.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if num % 5 == 0 and num % 11 == 0:
+    print("Divisible by both 5 and 11 - divisible by 5 and11.py:4")
+else:
+    print("Not divisible by both - divisible by 5 and11.py:6")
