b65d7cf4d4b5a97b9dce56f11ce1a003f474115c
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:19:21 +0530

    Add files via upload

---
+++ arithmetic_operator_selflearning/simple interest.py
@@ -0,0 +1,6 @@
+P = float(input("Enter Principal: "))
+R = float(input("Enter Rate: "))
+T = float(input("Enter Time: "))
+
+SI = (P * R * T) / 100
+print("Simple Interest is: - simple interest.py:6", SI)
