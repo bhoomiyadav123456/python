050ca88ef816b933944dfe7cc3852e5bcbf64148
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:59:36 +0530

    Add files via upload

---
+++ selection_statement_selflearning/grade.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A - grade.py:4")
+elif marks >= 75:
+    print("Grade B - grade.py:6")
+elif marks >= 50:
+    print("Grade C - grade.py:8")
+else:
+    print("Fail - grade.py:10")
