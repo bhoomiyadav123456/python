3587de9909a7cba6d33dd5620d477439f2c0530f
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-19 23:47:06 +0530

    Add files via upload

---
+++ selection_statement_selflearning/greatest of 3 num.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b and a > c:
+    print("First number is greatest - greatest of 3 num.py:6")
+elif b > c:
+    print("Second number is greatest - greatest of 3 num.py:8")
+else:
+    print("Third number is greatest - greatest of 3 num.py:10")
