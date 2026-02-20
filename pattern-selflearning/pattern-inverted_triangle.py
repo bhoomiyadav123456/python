5fbc8bcc2672cf84e3ed8efc056e4af4d75cba53
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-21 00:13:40 +0530

    Add files via upload

---
+++ pattern-selflearning/pattern-inverted_triangle.py
@@ -0,0 +1,12 @@
+n = 5   # number of rows
+
+for i in range(n, 0, -1):
+    print("* - pattern-inverted_triangle.py:4" * i)
+
+    #output:
+    # * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4
+    # * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4
+    # * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4
+    # * - pattern-inverted_triangle.py:4 * - pattern-inverted_triangle.py:4
+    # * - pattern-inverted_triangle.py:4    
+        
