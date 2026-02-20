4bbe52d3996a3227d694d6f132e4328412c456f4
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-20 23:39:26 +0530

    Add files via upload

---
+++ class_work/moduleimport2.py
@@ -0,0 +1,19 @@
+#to import the module
+from numericcalculation import*
+
+
+#to display the addition of two numbers
+displayaddition(30,89)
+
+#to display the subtraction of two numbers
+displaysubtraction(80,40)
+
+#to display the multiplication of two numbers
+displayproduct(20,50)
+#to display the division of two numbers
+displaydivision(100,20)
+
+#output:
+#Addition of - numericcalculation.py:2 30  and  89 is  119
+#Subtraction of - numericcalculation.py:7 80  and  40 is  40
+#Product of - numericcalculation.py:12 20  and  50 is  1000
