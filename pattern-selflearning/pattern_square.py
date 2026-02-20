c4aed8074154626090c1acd1d545aa9210b20113
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-20 23:56:53 +0530

    Add files via upload

---
+++ pattern-selflearning/pattern_square.py
@@ -0,0 +1,12 @@
+n=5
+for i in range(n):
+    for j in range(n):
+        print("* - pattern_square.py:4", end=" ")
+    print()
+
+    #output:
+    # * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4
+    # * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4
+    # * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4
+    # * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4
+    # * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4  * - pattern_square.py:4
