182ec09a0b70f6b8427cae14342cd2564001ff6c
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-21 00:07:57 +0530

    Add files via upload

---
+++ pattern-selflearning/pattern_rectangle.py
@@ -0,0 +1,12 @@
+row=4
+col=5
+for i in range(row):
+    for j in range(col):
+        print("* - pattern_rectangle.py:5", end=" ")
+    print()
+
+    #output:
+    # * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5    
+    # * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5
+    # * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5
+    # * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5  * - pattern_rectangle.py:5
