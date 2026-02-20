cb19b0e56fa7551551964c06c78b2b69565db9d7
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-20 23:40:19 +0530

    Add files via upload

---
+++ class_work/numericcalculation.py
@@ -0,0 +1,39 @@
+def displayaddition(a,b):
+   print("Addition of - numeric_calculation.py:2",a," and ",b,"is ",(a+b))
+   def calculationaddition(a,b):
+          return (a+b)
+   
+   def displaysubtraction(a,b):
+      print("Subtraction of - numeric_calculation.py:7",a," and ",b,"is ",(a-b))
+      def calculationsubtraction(a,b):
+          return (a-b)
+      
+      def displayproduct(a,b):
+         print("Product of - numeric_calculation.py:12",a," and ",b,"is ",(a*b))
+         def calculationproduct(a,b):
+             return (a*b)
+         
+         def displaydivision(a,b):
+             if b==0:
+                 print("division is not allowed - numeric_calculation.py:18")
+             else:
+                 print("product of - numeric_calculation.py:20",a,"and ",b,"is ",(a/b))
+                 def calculationproduct(a,b):
+                     return(a/b)
+                 
+                 def calculateremainder(a,b):
+                     if b==0:
+                         return None
+                     return a%b
+                 
+                 #output:
+                    a=10
+                    b=5
+                    displayaddition(a,b)
+                    displaysubtraction(a,b)
+                    displayproduct(a,b)
+                    displaydivision(a,b)
+
+                     
+           
+          
