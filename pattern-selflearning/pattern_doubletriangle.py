3720b53a81ef718e5c0ef2125cf4f6e8e46bb11f
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-21 22:29:18 +0530

    Add files via upload

---
+++ pattern-selflearning/pattern_doubletriangle.py
@@ -0,0 +1,11 @@
+n = 5
+for i in range(1, n+1):
+    print("* - pattern_doubletriangle.py:3"*i + " "*(n-i) + "*"*(n-i))
+
+    #output:
+    # *    ****
+    # **   ***
+    # ***  **
+    # **** *
+    # *****
+        
