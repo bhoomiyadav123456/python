6e2933e918b6422b70c0400f84c71138ce212dea
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 00:03:24 +0530

    This program use isalpha()

---
+++ class_work_23feb/ex_dict_1.py
@@ -0,0 +1,15 @@
+sentence = "hello word"
+print(sentence.isalpha())
+str2 = "hello"
+print(str2.isalpha())
+str3 = "hello123"
+print(str3.isalnum())
+
+str4 = "123"
+print(str4.isalnum())
+
+#output:
+# False
+# True
+# False
+# True
