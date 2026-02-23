8313e9968b33d0e9f3ce819e34c4952f2e9669a4
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 22:21:18 +0530

    This program contains basic set operation.

---
+++ class_work_23feb/setinpython.py
@@ -0,0 +1,14 @@
+set1 = {45,67,89,45,67,89,34,90,45,59,120,34,90,45,59,120}
+print("set is: - seinpython.py:2")
+print(set1)
+#to insert 34 in set
+set1.add(34)
+print("After inserting 34 in set: - seinpython.py:6")
+print(set1)
+
+#output:
+# set is: - seinpython.py:2
+# {34, 45, 67, 89, 90, 120, 59} 
+# After inserting 34 in set: - seinpython.py:6
+# {34, 45, 67, 89, 90, 120, 59}
+    
