f173a2bacb758afc5b9e45d8721739c34c18b31f
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:47:57 +0530

    This program demonstrate tuple indexing and slicing.

---
+++ selflearning_23feb/tuple_operations.py
@@ -0,0 +1,10 @@
+t = (1, 2, 3, 4, 5)
+
+print("First element: - tuple_operations.py:3", t[0])
+print("Last element: - tuple_operations.py:4", t[-1])
+print("Slice (1 to 3): - tuple_operations.py:5", t[1:4])
+
+#output:
+# First element: - tuple_operations.py:3 1
+# Last element: - tuple_operations.py:4 5
+# Slice (1 to 3): - tuple_operations.py:5 (2, 3, 4)
