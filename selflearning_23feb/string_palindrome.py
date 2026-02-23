84de291de6beabbb72bbf9a75def4bf550242bc4
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:32:46 +0530

    This program is used to check whether a string is a palindrome.

---
+++ selflearning_23feb/string_palindrome.py
@@ -0,0 +1,12 @@
+text = input("Enter a string: ")
+
+if text == text[::-1]:
+    print("Palindrome - string_palindrome.py:4")
+else:
+    print("Not Palindrome - string_palindrome.py:6")
+
+    #output:
+# Enter a string: madam
+# Palindrome - string_palindrome.py:4
+# Enter a string: hello
+# Not Palindrome - string_palindrome.py:6   
