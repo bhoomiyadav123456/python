a68856697dd6dbab3804d4801776cce20ae82266
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:27:12 +0530

    This program Traverse and print dictionary keys and values.

---
+++ selflearning_23feb/dictionary_traversal.py
@@ -0,0 +1,9 @@
+marks = {"Math": 85, "English": 90, "Science": 88}
+
+for key in marks:
+    print(key, ": - dictionary_traversal.py:4", marks[key])
+
+    #output:
+# Math : - dictionary_traversal.py:4 85
+# English : - dictionary_traversal.py:4 90
+# Science : - dictionary_traversal.py:4 88
