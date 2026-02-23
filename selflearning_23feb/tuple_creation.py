70c2f4f386cb2b11f777428d446e8933674d9c6c
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:45:57 +0530

    This program is used to create and display elements of a tuple.

---
+++ selflearning_23feb/tuple_creation.py
@@ -0,0 +1,12 @@
+t = (10, 20, 30, 40)
+print("Tuple elements: - tuple_creation.py:2")
+for i in t:
+    print(i)
+
+    #output:
+# Tuple elements: - tuple_creation.py:2
+# 10
+# 20    
+# 30
+# 40
+    
