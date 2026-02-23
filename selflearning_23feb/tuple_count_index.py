5392ed11a78a28b6436fcb2e19cdcebb350c2f65
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:51:44 +0530

    This program is used to find frequency and index of an element in a tuple.

---
+++ selflearning_23feb/tuple_count_index.py
@@ -0,0 +1,8 @@
+t = (5, 10, 15, 5, 20, 5)
+
+print("Count of 5: - tuple_count_index.py:3", t.count(5))
+print("Index of 15: - tuple_count_index.py:4", t.index(15))
+
+#output:
+# Count of 5: - tuple_count_index.py:3 3
+# Index of 15: - tuple_count_index.py:4 2
