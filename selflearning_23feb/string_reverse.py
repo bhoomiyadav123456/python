55dec73896c04a5e4e9563f0707ad790e45e36cb
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-23 23:35:06 +0530

    This program to reverse a string using slicing.

---
+++ selflearning_23feb/string_reverse.py
@@ -0,0 +1,8 @@
+text = input("Enter a string: ")
+
+reversed_text = text[::-1]
+
+print("Reversed string: - string_reverse.py:5", reversed_text)
+#output:
+# Enter a string: Hello, World!
+# Reversed string: - string_reverse.py:5 !dlroW ,olleH
