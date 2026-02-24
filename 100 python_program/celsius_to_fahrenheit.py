03947db2f53f7630983369e5d7b16a1c84cccc43
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:01:30 +0530

    PB6 convert temperature from Celsius to Fahrenheit.

---
+++ 100 python_program/celsius_to_fahrenheit.py
@@ -0,0 +1,11 @@
+# Read input
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit is: - celsius_to_fahrenheit.py:7", fahrenheit)
+
+#output:
+Enter temperature in Celsius: 25
+Temperature in Fahrenheit is: 77.0
