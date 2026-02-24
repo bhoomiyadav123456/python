081d5535e371d55d3b433c5731b4297194f9c782
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 10:33:47 +0530

    PB1 swap two numbers without using third variable

---
+++ 100 python_program/swap.py
@@ -0,0 +1,12 @@
+# Read input
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Swapping without third variable
+a = a + b
+b = a - b
+a = a - b
+print(a)
+print(b)
+
+#output:
