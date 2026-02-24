1b950b92317572a78a75580d7ac58730bdd89d46
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 16:18:53 +0530

    PB98 Check whether string contains only digits.

---
+++ 100 python_program/check_digits.py
@@ -0,0 +1,11 @@
+s = input("Enter string: ")
+
+if s.isdigit():
+    print("Contains only digits - check_digits.py:4")
+else:
+    print("Does not contain only digits - check_digits.py:6")
+
+
+    '''Output:'''
+    Enter string: 12345
+    Contains only digits
