f448834d2243149f1f80365321011bd523019547
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 15:46:31 +0530

    PB87 Check whether string is palindrome.

---
+++ 100 python_program/string_palindrome.py
@@ -0,0 +1,11 @@
+s = input("Enter string: ")
+
+if s == s[::-1]:
+    print("Palindrome - string_palindrome.py:4")
+else:
+    print("Not Palindrome - string_palindrome.py:6")
+
+
+    '''Output:'''
+    Enter string: madam
+    Palindrome
