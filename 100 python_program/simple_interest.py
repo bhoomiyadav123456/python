0d1d120ec9a41a22910f04dacd2c5bc8400cc15b
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 10:53:23 +0530

    PB4 calculate simplw

---
+++ 100 python_program/simple_interest.py
@@ -0,0 +1,16 @@
+# Read input from standard input
+principal = float(input())
+rate = float(input())
+time = float(input())
+
+# Calculate Simple Interest
+simple_interest = (principal * rate * time) / 100
+
+# Print the required output
+print(simple_interest)
+
+#output:
+Enter principal amount: 1000
+Enter rate of interest: 5
+Enter time: 2
+Simple Interest is: 100.0
