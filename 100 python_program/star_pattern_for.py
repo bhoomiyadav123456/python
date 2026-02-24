ba59291693afc520f5f5be1ebaf4f636b3d1345a
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 13:15:41 +0530

    PB33 Print star pattern using for loop.

---
+++ 100 python_program/star_pattern_for.py
@@ -0,0 +1,15 @@
+# Read input
+n = int(input("Enter number of rows: "))
+
+# Print star pattern
+for i in range(1, n + 1):
+    print("* - star_pattern_for.py:6" * i)
+
+
+'''Output:'''
+Enter number of rows: 5
+*
+**
+***
+****
+***** 
