7a2111706315ad90699c220de921c78406803261
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:37:59 +0530

    PB15 Check whether a number is divisible by both 3 and 5.

---
+++ 100 python_program/divisible_by_3_and_5.py
@@ -0,0 +1,13 @@
+# Read input
+num = int(input("Enter a number: "))
+
+# Check divisibility by 3 and 5
+if num % 3 == 0 and num % 5 == 0:
+    print("Divisible by 3 and 5 - divisible_by_3_and_5.py:6")
+else:
+    print("Not divisible by 3 and 5 - divisible_by_3_and_5.py:8")
+
+
+    '''output:'''
+    Enter a number: 56
+    Not divisible by 3 and 5
