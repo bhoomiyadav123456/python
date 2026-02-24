3f0591b973eb95414f4d3d4cbe2ce4642e53b156
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 14:58:50 +0530

    PB68 Convert tuple to list.

---
+++ 100 python_program/tuple_to_list.py
@@ -0,0 +1,12 @@
+# Read input
+numbers = tuple(map(int, input("Enter numbers separated by space: ").split()))
+
+converted_list = list(numbers)
+
+print("Converted list: - tuple_to_list.py:6", converted_list)
+
+
+
+'''Output:'''
+Enter numbers separated by space: 1 2 3 4
+Converted list: [1, 2, 3, 4]
