47c3dc2ea0a7c154eae35ccf15dc49a4ba18299c
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 12:34:19 +0530

    PB28 Print multiplication table using while loop.

---
+++ 100 python_program/multiplication_table.py
@@ -0,0 +1,27 @@
+# Read input
+n = int(input("Enter a number: "))
+
+# Print multiplication table using while loop
+i = 1
+
+print("Multiplication Table: - multiplication_table.py:7")
+
+while i <= 10:
+    print(n, "x - multiplication_table.py:10", i, "=", n * i)
+    i += 1
+
+
+
+    '''output:'''
+    Enter a number: 5
+    Multiplication Table:
+    5 x 1 = 5
+    5 x 2 = 10
+    5 x 3 = 15
+    5 x 4 = 20
+    5 x 5 = 25
+    5 x 6 = 30
+    5 x 7 = 35
+    5 x 8 = 40
+    5 x 9 = 45
+    5 x 10 = 50
