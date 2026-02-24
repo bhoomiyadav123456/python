7c73087a972e690f581b9d9fc4928f7cec6743d7
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:30:45 +0530

    PB13 Check whether a character is vowel or consonant.

---
+++ 100 python_program/vowel_consonant.py
@@ -0,0 +1,15 @@
+# Read input
+ch = input("Enter a character: ").lower()
+
+# Check vowel or consonant
+if ch in 'aeiou':
+    print("Vowel - vowel_consonant.py:6")
+else:
+    print("Consonant - vowel_consonant.py:8")
+
+
+    '''output:'''
+    Enter a character: h
+    Consonant
+    Enter a character: a
+    Vowel
