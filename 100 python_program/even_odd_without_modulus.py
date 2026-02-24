38c99e3dd03fc027540670a4a7081aa98b284a6f
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:07:50 +0530

    PB8 Check a number is even or odd without modulus operator.

---
+++ 100 python_program/even_odd_without_modulus.py
@@ -0,0 +1,13 @@
+# Read input
+num = int(input("Enter a number: "))
+
+# Check even or odd without using modulus operator
+if num // 2 * 2 == num:
+    print("Even - even_odd_without_modulus.py:6")
+else:
+    print("Odd - even_odd_without_modulus.py:8")
+
+
+    #output:
+    Enter a number: 6
+    Even
