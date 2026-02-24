d98d888cc43a817a22373fdf6e2563cac4e39b57
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:59:14 +0530

    PB21 Print numbers from 1 to N using while loop.

---
+++ 100 python_program/print_1_to_n.py
@@ -0,0 +1,17 @@
+# Read input
+n = int(input("Enter value of N: "))
+
+# Print numbers from 1 to N using while loop
+i = 1
+while i <= n:
+    print(i)
+    i += 1
+
+
+    '''OUTPUT:'''
+    Enter value of N: 5
+    1
+    2
+    3
+    4
+    5
