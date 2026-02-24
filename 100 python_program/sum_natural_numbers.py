0797d3763a31ed2aeb05cf0ffc4f74147de98002
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 13:18:09 +0530

    PB34 Find sum of first N natural numbers.

---
+++ 100 python_program/sum_natural_numbers.py
@@ -0,0 +1,17 @@
+# Read input
+n = int(input("Enter value of N: "))
+
+# Calculate sum of first N natural numbers
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+# Output
+print("Sum is: - sum_natural_numbers.py:11", total)
+
+
+
+'''Output:'''
+Enter value of N: 5
+Sum is: 15
