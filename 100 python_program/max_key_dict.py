b7005f4d93c74c5babda7192615b761ee1c073f0
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 15:30:28 +0530

    PB81 Find key with maximum value.

---
+++ 100 python_program/max_key_dict.py
@@ -0,0 +1,8 @@
+d = eval(input("Enter dictionary: "))
+key = max(d, key=d.get)
+print("Key with maximum value: - max_key_dict.py:3", key)
+
+
+'''Output:'''
+Enter dictionary: {'a':10,'b':25,'c':15}
+Key with maximum value: b
