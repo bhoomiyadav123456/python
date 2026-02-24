86d0de62da44929d5c4123cdafd5914f947f3dc8
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 13:36:06 +0530

    PB38 Reverse a string using for loop.

---
+++ 100 python_program/reverse_string_for.py
@@ -0,0 +1,16 @@
+# Read input
+string = input("Enter a string: ")
+
+# Reverse string
+rev = ""
+
+for ch in string:
+    rev = ch + rev
+
+# Output
+print("Reversed string is: - reverse_string_for.py:11", rev)
+
+
+'''Output:'''
+Enter a string: hello
+Reversed string is: olleh
