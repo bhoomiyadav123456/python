d60d535a8a349926c236df29249f819edfc2cbab
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 15:43:35 +0530

    PB86 Reverse a string without slicing.

---
+++ 100 python_program/reverse_string.py
@@ -0,0 +1,13 @@
+s = input("Enter string: ")
+rev = ""
+
+for ch in s:
+    rev = ch + rev
+
+print("Reversed string: - reverse_string.py:7", rev)
+
+
+
+'''Output:'''
+Enter string: hello
+Reversed string: olleh
