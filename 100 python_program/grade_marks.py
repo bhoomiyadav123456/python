3c6f9b13f23cdffb68f67f4d00d4fffee9485775
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-02-24 11:26:46 +0530

    PB12 Assign grades based on marks.

---
+++ 100 python_program/grade_marks.py
@@ -0,0 +1,19 @@
+# Read input
+marks = int(input("Enter marks: "))
+
+# Assign grade based on marks
+if marks >= 90:
+    print("Grade A - grade_marks.py:6")
+elif marks >= 80:
+    print("Grade B - grade_marks.py:8")
+elif marks >= 70:
+    print("Grade C - grade_marks.py:10")
+elif marks >= 60:
+    print("Grade D - grade_marks.py:12")
+else:
+    print("Fail - grade_marks.py:14")
+
+
+    '''output:'''
+    Enter marks: 95
+    Grade A 
