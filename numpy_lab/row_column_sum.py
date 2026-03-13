636bcdb60f678230ae44706839565a8014f4e649
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-03-13 15:51:05 +0530

    Calculate row-wise and column-wise sums of a 4×4 matrix

---
+++ numpy_lab/row_column_sum.py
@@ -0,0 +1,14 @@
+import numpy as np
+matrix = np.array([[1,2,3,4],
+                   [5,6,7,8],
+                   [9,10,11,12],
+                   [13,14,15,16]])
+row_sum = np.sum(matrix,axis=1)
+column_sum = np.sum(matrix,axis=0)
+print("Row Sum: - row_column_sum.py:8",row_sum)
+print("Column Sum: - row_column_sum.py:9",column_sum)
+
+
+'''output'''
+Row Sum: [10 26 42 58]
+Column Sum: [28 32 36 40]
