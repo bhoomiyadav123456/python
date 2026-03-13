8b255d7cbee404ac25c713fbe277e0742bdd2d6f
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-03-13 22:38:17 +0530

    Create statistical functions

---
+++ class_work_13_march/statistical_functions.py
@@ -0,0 +1,14 @@
+import numpy as np
+array1 = np.array([32,56,78,90,45,89,12,13])
+print("First array - statistical_functions.py:3")
+print(array1)
+average = np.mean(array1)
+print(average)
+print("standard deviation : - statistical_functions.py:7" , np.std(array1))
+
+
+'''output'''
+First array
+[32 56 78 90 45 89 12 13]
+51.875
+standard deviation : 29.7381804251706
