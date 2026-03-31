348c90a468ad75613051603eb696769ddb140d6c
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-03-31 10:54:08 +0530

    Find mean, median, and standard deviation .

---
+++ python lab + assesment/numpy_stats.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Mean: - numpy_stats.py:5", np.mean(arr))
+print("Median: - numpy_stats.py:6", np.median(arr))
+print("Std Dev: - numpy_stats.py:7", np.std(arr))
+
+
+'''Output'''
+Mean: 30.0
