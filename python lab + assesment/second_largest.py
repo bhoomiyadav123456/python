4928a89ccd654108d50ca013c96d94716ffe0e2b
bhoomiyadav123456 <bhoomi.yadav.mca.2025@miet.ac.in>
2026-03-31 10:16:08 +0530

    In this program finding the second largest no. .

---
+++ python lab + assesment/second_largest.py
@@ -0,0 +1,16 @@
+# Find second largest without using sort
+
+nums = [10, 20, 4, 45, 99]
+
+largest = second = float('-inf')
+
+for num in nums:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Second Largest: - second_largest.py:14", second)
+
+
